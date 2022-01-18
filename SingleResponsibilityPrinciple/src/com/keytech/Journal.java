96d318409356b05df2d6723685ee1ae847b9cce6
benNjogu <kamandemachafi.gmail.com>
2022-01-18 10:53:43 +0300

    Breaking the principle by adding persistence functions

--- SingleResponsibilityPrinciple/src/com/keytech/Journal.java
+++ SingleResponsibilityPrinciple/src/com/keytech/Journal.java
@@ -1,5 +1,8 @@
 package com.keytech;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,16 +11,31 @@ public class Journal {
 	private static int count = 0;
 
 	public void addEntry(String text) {
-		entries.add(""+(++count)+": "+text);
+		entries.add("" + (++count) + ": " + text);
 	}
-	
+
 	public void removeEntry(int index) {
-		entries.remove(index); 
+		entries.remove(index);
 	}
 
 	@Override
 	public String toString() {
 		return String.join(System.lineSeparator(), entries);
 	}
-	
+
+	public void save(String filename) {
+		try (PrintStream out = new PrintStream(filename)) {
+			out.println(toString());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void load(String filename) {
+
+	}
+
+	public void load(URL url) {
+
+	}
 }
