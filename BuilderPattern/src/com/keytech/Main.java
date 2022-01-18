0a8a4c7e3aa4a261537000c326244472585856cb
benNjogu <kamandemachafi.gmail.com>
2022-01-18 17:10:13 +0300

    The builder pattern

---
+++ BuilderPattern/src/com/keytech/Main.java
@@ -0,0 +1,13 @@
+package com.keytech;
+
+public class Main {
+
+	/*
+	 * Builder :- When pieceWice object construction is complicated, 
+	 *  provide an API for doing it succinctly(briefly and to the point)
+	 * */
+	public static void main(String[] args) {
+		
+	}
+	
+}
