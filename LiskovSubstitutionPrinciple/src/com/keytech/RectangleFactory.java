0245dc65e0c8b8722a44f315ddaf31093a306d8b
benNjogu <kamandemachafi.gmail.com>
2022-01-18 15:43:48 +0300

    Adding rectangle factory class to handle both

---
+++ LiskovSubstitutionPrinciple/src/com/keytech/RectangleFactory.java
@@ -0,0 +1,12 @@
+package com.keytech;
+
+public class RectangleFactory {
+
+	public static Rectangle newRectangle(int width, int height) {
+		return newRectangle(width, height);
+	}
+
+	public static Rectangle newSquare(int side) {
+		return newRectangle(side, side);
+	}
+}
