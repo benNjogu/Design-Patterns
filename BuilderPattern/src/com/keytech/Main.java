66a9fa47caad08604d9fb2cc250f7445bbe1dba7
benNjogu <kamandemachafi.gmail.com>
2022-01-19 10:48:58 +0300

    Using string builder.

--- BuilderPattern/src/com/keytech/Main.java
+++ BuilderPattern/src/com/keytech/Main.java
@@ -8,6 +8,20 @@ public class Main {
 	 * */
 	public static void main(String[] args) {
 		
+		String hello = "hello";
+		
+		System.out.println("<p>"+hello+"<p>");
+		
+		String[] words = {"hello","world"};
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<ul>\n");
+		for (String word : words) {
+			sb.append(String.format("  <li>%s</li>\n", word));
+		}
+		sb.append("</ul>");
+		System.out.println(sb.toString());
+		
 	}
 	
 }
