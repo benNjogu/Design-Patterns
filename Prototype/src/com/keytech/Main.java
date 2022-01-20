69d91bd4899f73694338f9084058738285150c85
benNjogu <kamandemachafi.gmail.com>
2022-01-20 17:57:14 +0300

    Prototype defination

---
+++ Prototype/src/com/keytech/Main.java
@@ -0,0 +1,15 @@
+package com.keytech;
+
+
+
+public class Main {
+
+	/*
+	 * A partially or fully initialized object that you copy(clone) 
+	 * and make use of.
+	 * */
+	public static void main(String[] args) {
+		
+	}
+	
+}
