c5c6f7a5db923414799948684578f469c3c439dd
benNjogu <kamandemachafi.gmail.com>
2022-01-20 14:20:41 +0300

    Defining a factory

---
+++ Factories/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	/*
+	 * Factory :- A component responsible solely for the wholesale(not piecewise) creation of objects
+	 * */
+	public static void main(String[] args) {
+
+	}
+
+}
