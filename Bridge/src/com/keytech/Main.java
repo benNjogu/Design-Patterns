2a57086b77fab8a140636a636f00237d229f0e22
benNjogu <kamandemachafi.gmail.com>
2022-01-21 17:07:31 +0300

    Bridge defination

---
+++ Bridge/src/com/keytech/Main.java
@@ -0,0 +1,13 @@
+package com.keytech;
+
+public class Main {
+
+	/*
+	 * Bridge:- An interface that decouples an interface(hierarchy) 
+	 * from an implementation (hierarchy)
+	 * */
+	public static void main(String[] args) {
+		
+	}
+	
+}
