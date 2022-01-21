fa884a996eec10bc297a1febd95cdfe73d0d6e16
benNjogu <kamandemachafi.gmail.com>
2022-01-21 16:21:54 +0300

    Adapter defination

---
+++ Adapter/src/com/keytech/Main.java
@@ -0,0 +1,13 @@
+package com.keytech;
+
+public class Main {
+
+	/*
+	 * Adapter:- A construct which adapts an existing interface X 
+	 * to conform to the required interface Y.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
