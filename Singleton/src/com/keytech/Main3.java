c00b3f5d19bd177263c11f938c912a70d4c93f6a
benNjogu <kamandemachafi.gmail.com>
2022-01-21 12:10:02 +0300

    Problems with singleton

---
+++ Singleton/src/com/keytech/Main3.java
@@ -0,0 +1,13 @@
+package com.keytech;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		/*
+		 * Two problems with singleton :- 
+		 * 1. Lazy initialization 
+		 * 2. Thread safety
+		 * */
+	}
+	
+}
