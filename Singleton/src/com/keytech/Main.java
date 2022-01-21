7639dbf11d99a28a5b7e4fd16cd18e275329d59b
benNjogu <kamandemachafi.gmail.com>
2022-01-21 10:52:21 +0300

    A very basic singleton

--- Singleton/src/com/keytech/Main.java
+++ Singleton/src/com/keytech/Main.java
@@ -1,12 +1,40 @@
 package com.keytech;
 
+class BasicSingleton {
+
+	private BasicSingleton() {
+
+	}
+
+	private static final BasicSingleton INSTANCE = new BasicSingleton();
+
+	public static BasicSingleton getInstance() {
+		return INSTANCE;
+	}
+
+	private int value = 0;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
+
 public class Main {
 
 	/*
 	 * Singleton :- a component which is instantiated only once
-	 * */
+	 */
 	public static void main(String[] args) {
+
+		BasicSingleton singleton = BasicSingleton.getInstance();
+		singleton.setValue(23);
+		System.out.println(singleton.getValue());
 		
 	}
-	
+
 }
