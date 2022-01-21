7a5271ae314a1a585950ca65e75a7634aa4096a6
benNjogu <kamandemachafi.gmail.com>
2022-01-21 12:19:03 +0300

    Lazy initialization with thread safety problem

--- Singleton/src/com/keytech/Main3.java
+++ Singleton/src/com/keytech/Main3.java
@@ -1,13 +1,31 @@
 package com.keytech;
 
+class LazySingleton {
+
+	private static LazySingleton instance;
+
+	private LazySingleton() {
+		System.out.println("Initializing a lazy singleton");
+	}
+
+	public static LazySingleton getInstance() {
+
+		if (instance == null) {
+			
+			instance = new LazySingleton();
+		}
+
+		return instance;
+	}
+
+}
+
 public class Main3 {
 
 	public static void main(String[] args) {
 		/*
-		 * Two problems with singleton :- 
-		 * 1. Lazy initialization 
-		 * 2. Thread safety
-		 * */
+		 * Two problems with singleton :- 1. Lazy initialization 2. Thread safety
+		 */
 	}
-	
+
 }
