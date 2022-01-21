f0cbeb1e38b5d74297ae377e12847d46ca20898b
benNjogu <kamandemachafi.gmail.com>
2022-01-21 11:28:40 +0300

    Serialization and deserialization

--- Singleton/src/com/keytech/Main.java
+++ Singleton/src/com/keytech/Main.java
@@ -1,6 +1,12 @@
 package com.keytech;
 
-class BasicSingleton {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class BasicSingleton implements Serializable{
 
 	private BasicSingleton() {
 
@@ -29,14 +35,41 @@ public class Main {
 	/*
 	 * Singleton :- a component which is instantiated only once
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		/*
-		 * The two problems with singleton
-		 * 1. Reflection
-		 * 2. Serialization
-		 * */
+		BasicSingleton singleton = BasicSingleton.getInstance();
+		singleton.setValue(111);
 		
+		String fileName = "singleton.bin";
+		saveToFile(singleton, fileName);
+		
+		singleton.setValue(222);
+		
+		BasicSingleton singleton2 = readFromFile(fileName);
+		
+		System.out.println(singleton2 == singleton);
+		System.out.println(singleton.getValue());
+		System.out.println(singleton2.getValue());
+
+	}
+
+	static void saveToFile(BasicSingleton singleton, String fileName) throws Exception {
+
+		try (FileOutputStream fileOut = new FileOutputStream(fileName);
+				ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+			out.writeObject(singleton);
+		}
+
+	}
+
+	static BasicSingleton readFromFile(String fileName) throws Exception {
+
+		try (FileInputStream fileIn = new FileInputStream(fileName);
+				ObjectInputStream in = new ObjectInputStream(fileIn)) {
+
+			return (BasicSingleton) in.readObject();
+		}
+
 	}
 
 }
