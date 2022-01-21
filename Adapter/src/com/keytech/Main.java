5f9769a6eddbfbf80fe69ff263996b4f630f53b1
benNjogu <kamandemachafi.gmail.com>
2022-01-21 16:42:13 +0300

    The scenario

--- Adapter/src/com/keytech/Main.java
+++ Adapter/src/com/keytech/Main.java
@@ -1,11 +1,69 @@
 package com.keytech;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Point {
+
+	private int x, y;
+
+	public Point(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+}
+
+class Line {
+
+	public Point start, end;
+
+	public Line(Point start, Point end) {
+		super();
+		this.start = start;
+		this.end = end;
+	}
+
+}
+
+class VectorObject extends ArrayList<Line> {
+
+}
+
+class VectorRectangle extends VectorObject {
+
+	public VectorRectangle(int x, int y, int width, int height) {
+
+		add(new Line(new Point(x, y), new Point((x + width), y)));
+		add(new Line(new Point((x + width), y), new Point((x + width), (y + height))));
+		add(new Line(new Point(x, y), new Point((x + width), (y + height))));
+		add(new Line(new Point(x, (y + height)), new Point((x + width), (y + height))));
+	}
+}
+
 public class Main {
 
 	/*
-	 * Adapter:- A construct which adapts an existing interface X 
-	 * to conform to the required interface Y.
+	 * Adapter:- A construct which adapts an existing interface X to conform to the
+	 * required interface Y.
 	 */
+
+	private static final List<VectorObject> VECTOR_OBJECTS = new ArrayList<>(
+			Arrays.asList(new VectorRectangle(1, 1, 10, 10), 
+					new VectorRectangle(3, 3, 6, 6)));
+	
+	public static void drawPoint(Point point) {
+		
+		System.out.println(".");
+	}
+
 	public static void main(String[] args) {
 
 	}
