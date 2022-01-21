4ad2f100820c78088b3c1ca9f20271a4341f053d
benNjogu <kamandemachafi.gmail.com>
2022-01-21 14:32:20 +0300

    Accessing the object

--- Singleton/src/com/keytech/Main4.java
+++ Singleton/src/com/keytech/Main4.java
@@ -2,18 +2,29 @@ package com.keytech;
 
 class InnerStaticSingleton {
 
-	private InnerStaticSingleton() {
+	private static int age;
+	
+	private InnerStaticSingleton(int age) {
 
+		this.age = age;
+	}
+
+	public static int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	private static class Impl {
 
 		private static final InnerStaticSingleton INSTANCE = 
-				new InnerStaticSingleton();
+				new InnerStaticSingleton(age);
 
 	}
 	
-	public InnerStaticSingleton getInstance() {
+	public static InnerStaticSingleton getInstance() {
 		
 		return Impl.INSTANCE;	
 	}
@@ -21,4 +32,11 @@ class InnerStaticSingleton {
 
 public class Main4 {
 
+	public static void main(String[] args) {
+		
+		InnerStaticSingleton singleton = InnerStaticSingleton.getInstance();
+		singleton.setAge(23);
+		System.out.println(singleton.getAge());
+	}
+	
 }
