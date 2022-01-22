c9694959b4b33edb8b22c33fddfede8226247fef
benNjogu <kamandemachafi.gmail.com>
2022-01-22 12:42:24 +0300

    No checks perfomed

--- Proxy/src/com/keytech/Main.java
+++ Proxy/src/com/keytech/Main.java
@@ -1,11 +1,66 @@
 package com.keytech;
 
+interface Drivable {
+
+	void drive();
+}
+
+class Car implements Drivable {
+
+	protected Driver driver;
+
+	public Car(Driver driver) {
+		super();
+		this.driver = driver;
+	}
+
+	@Override
+	public void drive() {
+
+		System.out.println("Car being driven");
+	}
+
+}
+
+class Driver {
+
+	public int age;
+
+	public Driver(int age) {
+		super();
+		this.age = age;
+	}
+
+}
+
+class CarProxy extends Car {
+
+	public CarProxy(Driver driver) {
+		super(driver);
+
+	}
+
+	@Override
+	public void drive() {
+
+		if (driver.age >= 16)
+			super.drive();
+		else
+			System.out.println("Driver too young");
+
+	}
+
+}
+
 public class Main {
 
 	/*
 	 * Proxy:- An interface for accessing a particular resource
-	 * */
+	 */
 	public static void main(String[] args) {
+
+		Car car = new Car(new Driver(12));
+		car.drive();
 		
 	}
 }
