2a811fda6ed80e45c6afa34457c2e81928adbff3
benNjogu <kamandemachafi.gmail.com>
2022-01-22 13:03:09 +0300

    Dynamic proxy

---
+++ Proxy/src/com/keytech/Demo2.java
@@ -0,0 +1,12 @@
+package com.keytech;
+
+public class Demo2 {
+
+	/*
+	 * Dynamic proxy:- created at runtime rather than compile time
+	 * */
+	public static void main(String[] args) {
+		
+	}
+	
+}
