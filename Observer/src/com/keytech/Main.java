73ab5c8539ff745f0f9f83efcfc04768c2fc4d2d
benNjogu <kamandemachafi.gmail.com>
2022-01-22 13:45:56 +0300

    Observer defination

---
+++ Observer/src/com/keytech/Main.java
@@ -0,0 +1,14 @@
+package com.keytech;
+
+public class Main {
+
+	/*
+	 *Observer:- An object that wishes to be informed about events happening 
+	 *in the system. 
+	 *The entity generating the events is an observable.
+	 * */
+	public static void main(String[] args) {
+		
+	}
+	
+}
