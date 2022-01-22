a8cdc78379214e73dabf6e92a065aedfb76784a8
benNjogu <kamandemachafi.gmail.com>
2022-01-22 11:45:59 +0300

    Decorator defination

---
+++ Decorator/src/com/keytech/Main.java
@@ -0,0 +1,14 @@
+package com.keytech;
+
+
+
+public class Main {
+
+	/*
+	 * Decorator:- Facilitate the addition of behaviors to individual objects
+	 * without inheriting from them.
+	 * */
+	public static void main(String[] args) {
+		
+	}
+}
