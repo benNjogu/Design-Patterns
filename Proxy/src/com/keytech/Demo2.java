afdde561fd5f4a5925fc1c0a32b3e9682ffa5438
benNjogu <kamandemachafi.gmail.com>
2022-01-22 13:34:11 +0300

    Dynamic proxy for logging

--- Proxy/src/com/keytech/Demo2.java
+++ Proxy/src/com/keytech/Demo2.java
@@ -1,5 +1,58 @@
 package com.keytech;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+
+interface Human{
+	
+	void walk();
+	void talk();
+}
+
+class LoggingHandler implements InvocationHandler{
+
+	private final Object target;
+	private Map<String, Integer> calls = new HashMap<String, Integer>();
+ 	
+	public LoggingHandler(Object target) {
+		super();
+		this.target = target;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] arg2) throws Throwable {
+		
+		String name = method.getName();
+		
+		if (name.contains("toString")) {
+			return calls.toString();
+		}
+		
+		calls.merge(name, 1, Integer::sum);
+		return method.invoke(target, arg2);
+	}
+	
+	
+}
+
+class Person implements Human{
+
+	@Override
+	public void walk() {
+		System.out.println("I am walking");
+	}
+
+	@Override
+	public void talk() {
+		System.out.println("I am talking");
+	}
+	
+	
+}
+
 public class Demo2 {
 
 	/*
@@ -7,6 +60,22 @@ public class Demo2 {
 	 * */
 	public static void main(String[] args) {
 		
+		Person person = new Person();
+		Human logged = withLogging(person, Human.class);
+		logged.walk();
+		logged.talk();
+		logged.talk();
+		System.out.println(logged);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T withLogging(T target, Class<T> itf) {
+		
+		return (T) Proxy.newProxyInstance(
+				itf.getClassLoader(),
+				new Class<?>[] {itf},
+				new LoggingHandler(target)
+				);
 	}
 	
 }
